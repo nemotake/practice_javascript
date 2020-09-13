04c5fbdfddf5d960a50cf1587ba7f9b0d5406f34
nemotake <wgenshou@gmail.com>
2020-09-13 12:36:34 +0000

    9_13_1

---
+++ test0.1.js
@@ -0,0 +1,38 @@
+var answer1 = prompt('正の値　or　負の値を入力してください');
+if (answer1 > 0) {
+  alert('正の値です');
+} else if (answer1 <0) {
+  alert('負の値です');
+} else {
+  alert('数値を入力してください');
+}
+
+var animals = ["パンダ" , "ライオン" ,"キリン"];
+var answer2 = prompt("好きな動物は何ですか？");
+var i = 0;
+while (i < animals.length) {
+    if(answer2 == "ライオン"){
+        alert(animals.index0f(1));
+        break;
+    }
+  alert(animals[i]);
+  i++; 
+}
+
+var animals2 = [
+  {'name': 'パンダ', 'weight': "80(kg)"},  // 0番目のオブジェクト変数
+  {'name': 'ライオン', 'weight': "200(kg)"},  // 1番目のオブジェクト変数
+  {'name': 'キリン', 'weight': "300(kg)"}   // 2番目のオブジェクト変数
+];
+var answer3 = prompt("体重を知りたい動物名を入力してください");
+if(answer3 == animals2.name){
+    alert(animals2.weight);
+}else{
+    alert("パンダ or ライオン　or　キリンを入力してください");
+}
+
+function calc(x) {
+  return x * x * 3.14;
+}
+
+alert(calc(8));
