65c1c2ae095e1ffc229a906a8d65bd8fcfae8519
nemotake <wgenshou@gmail.com>
2020-09-14 06:22:25 +0000

    9_14_1

--- test01.js
+++ test01.js
@@ -24,11 +24,15 @@ var animals2 = [
   {'name': 'ライオン', 'weight': "200(kg)"},  // 1番目のオブジェクト変数
   {'name': 'キリン', 'weight': "300(kg)"}   // 2番目のオブジェクト変数
 ];
-var answer3 = prompt("体重を知りたい動物名を入力してください");
-if(answer3 == animals2.name){
-    alert(animals2.weight);
+var answer3 = prompt("体重を知りたい動物名を入力してください。");
+if(answer3 == animals2[0].name){
+  alert(animals2[0].weight);
+}else if(answer3 == animals2[1].name){
+  alert(animals2[1].weight);
+}else if(answer3 == animals2[2].name){
+  alert(animals2[2].weight);
 }else{
-    alert("パンダ or ライオン or キリンを入力してください");
+  alert("パンダ or ライオン or キリンを入力してください");
 }
 
 function calc(x) {
--- test02.js
+++ test02.js
@@ -1,6 +1,18 @@
 (function($, window) {
   $(function() {
     // この中に処理を書きます
-    $('h1').html('jQueryの<span class="title">htmlメソッド</span>で書き換え');
+    $('h2').remove();
+    $('#index').append("<li>メソッド</li>");
+    
+      $('#index').find('li').each(function() {
+      // ループ内の処理
+      console.log($(this).text().length);
+      var mozi = $(this).text();
+      var mozi2 = mozi + "[" + mozi.length + "文字]";
+      $(this).text(mozi2);
+    });
+      
+    
+    
   });
 })(jQuery, window);
